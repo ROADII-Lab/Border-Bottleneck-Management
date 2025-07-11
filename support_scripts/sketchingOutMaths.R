7c64c56aca2283e62b84fe26a0a9b0dda016e0ce
nineveh-oconnell-volpe <nineveh.oconnell@dot.gov>
2025-07-11 10:40:36 -0400

    implemented skipping frames for better workflow

--- support_scripts/sketchingOutMaths.R
+++ support_scripts/sketchingOutMaths.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(ggplot2)
 
-in_dt <- fread("./code/Border-Bottleneck-Management/support_scripts/captured_coords/data_download_export_07102025_1648.csv")
+in_dt <- fread("./code/Border-Bottleneck-Management/support_scripts/captured_coords/data_download_export_07112025_1017.csv")
 
 # remove the nonnumeric characters from the confidence
 in_dt[, confidence := as.numeric(gsub("tensor\\(", "", gsub("\\)", "", confidence)))]
@@ -12,26 +12,24 @@ plot(in_dt$timestamp, in_dt$confidence)
 
 head(in_dt)
 
-#anyway... ok yeah, so we can see that no one identification of a vehicle ever got that far
-# remember, we're trying to track from 590 to 980 on the y scale to cover those 25 meters
-single_id <- 3
-ggplot(in_dt[id == single_id], aes(x = timestamp, y = cy)) +
-  geom_point()
-
-ggplot(in_dt[timestamp < 50000], aes(x = timestamp, y = cy, color = id)) +
+ggplot(in_dt, aes(x = timestamp, y = 1000-cy, color = id)) +
   geom_point()
 
 # ok i need help with this frame skipping thing
 # but generally the math I would want to do
 
 #classify lane
-in_dt[, lane := ifelse(cx < 400, "out of bounds", 
-                       ifelse(cx < 940, "left two", 
+in_dt[, lane := ifelse(cx < 400, "out_of_bounds", 
+                       ifelse(cx < 940, "left_two", 
                               ifelse(cx < 1100, "middle",
-                                     ifelse(cx < 1450, "right", "out of bounds"))))]
+                                     ifelse(cx < 1450, "right", "out_of_bounds"))))]
+
 # classify movement forward
 in_dt[, forward := ifelse(cy > 960, "early", 
-                          ifelse(cy > 590, "in zone", "past 25m segment"))]
+                          ifelse(cy > 590, "in_zone", "past_25m_segment"))]
+
+# what's the current run length
+in_dt[, max(timestamp)/1000]
 
 # let's take a peek at that
 ggplot(in_dt[timestamp < 50000], aes(x = cx, y = cy, color = forward)) +
@@ -41,3 +39,14 @@ dcast.data.table(in_dt, id ~ lane)
 # this video chunk didn't have much forward movement, but seems to like... work
 dcast.data.table(in_dt, id ~ forward)
 
+# get earliest timestamp in zone
+in_dt[, earliest_in_subsection := min(timestamp), .(id, class, forward)]
+highlights_dt <- in_dt[timestamp == earliest_in_subsection]
+
+# maybe we would also want to keep a count of how many times that object was identified as a sort 
+# of sanity check, have some kind of threshold for the confidence levels...
+wide_vehicle_timestamps <- dcast.data.table(highlights_dt, id + class + lane ~ forward, value.var = "earliest_in_subsection")
+wide_vehicle_timestamps[, sec_to_span_25m := (past_25m_segment - in_zone) / 1000 ]
+
+# it would have to be a vehicle that: started out in "early", wasn't out of bounds, made it eventually to "past 25 meter segment"
+# then just scale this time to the length of the bridge which is about 400 meters
